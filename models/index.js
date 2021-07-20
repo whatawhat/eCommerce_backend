e2200e3ad783569a6deb6e65a27a29e2f82eb5fc
Stephanie <whatawhat@hotmail.com>
2021-07-20 10:31:23 -0400

    created associations between models

--- models/index.js
+++ models/index.js
@@ -3,14 +3,29 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const { belongsToMany } = require('./Product');
 
 // Products belongsTo Category
+Products.belongsTo(Category, {
+  foreignKey: "product_id",
+  //onDelete: "CASCADE",
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+  //onDelete: "CASCADE",
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany (Tag, { 
+  through: "ProductTag"
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany (Product, {
+  through: "ProductTag"
+});
 
 module.exports = {
   Product,
