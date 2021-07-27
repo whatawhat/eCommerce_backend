c6eb5a22cc81050c866583fa09e530661d916415
Stephanie <whatawhat@hotmail.com>
2021-07-27 18:08:58 -0400

    uncommented out the console log to see the error

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -86,7 +86,7 @@ router.put('/:id', async (req, res) => {
       // get list of current tag_ids
       const productTagIds = productTags.map(({ tag_id }) => tag_id);
       // create filtered list of new tag_ids
-      const newProductTags = req.body.tag_id
+      const newProductTags = req.body.tagIds
         .filter((tag_id) => !productTagIds.includes(tag_id))
         .map((tag_id) => {
           return {
@@ -107,7 +107,7 @@ router.put('/:id', async (req, res) => {
     })
     .then((updatedProductTags) => res.json(updatedProductTags))
     .catch((err) => {
-      // console.log(err);
+      console.log(err);
       res.status(400).json(err);
     });
 });
