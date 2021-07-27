1552c42683572fad11b780738b50af72f3ae1ad3
Stephanie <whatawhat@hotmail.com>
2021-07-27 11:31:13 -0400

    changed tagIds to tag_id to match my code

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -51,8 +51,8 @@ router.post('/', async (req, res) => {
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
-        const productTagIdArr = req.body.tagIds.map((tag_id) => {
+      if (req.body.tag_id.length) {
+        const productTagIdArr = req.body.tag_id.map((tag_id) => {
           return {
             product_id: product.id,
             tag_id,
