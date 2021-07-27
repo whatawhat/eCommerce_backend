e1d7bd0121da9cf356f589cca5d4478708ec6927
Stephanie <whatawhat@hotmail.com>
2021-07-27 13:44:26 -0400

    changed tagIds to tag_id

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -86,7 +86,7 @@ router.put('/:id', async (req, res) => {
       // get list of current tag_ids
       const productTagIds = productTags.map(({ tag_id }) => tag_id);
       // create filtered list of new tag_ids
-      const newProductTags = req.body.tagIds
+      const newProductTags = req.body.tag_id
         .filter((tag_id) => !productTagIds.includes(tag_id))
         .map((tag_id) => {
           return {
@@ -96,7 +96,7 @@ router.put('/:id', async (req, res) => {
         });
       // figure out which ones to remove
       const productTagsToRemove = productTags
-        .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
+        .filter(({ tag_id }) => !req.body.tag_id.includes(tag_id))
         .map(({ id }) => id);
 
       // run both actions
